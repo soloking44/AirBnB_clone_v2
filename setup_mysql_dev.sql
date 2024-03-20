57c616fa2118286d1a9badcbdebfc0a7f5f7de89
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-03-20 15:40:16 +0100

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- It Sets up a MySQL server in preparation for the project.
+-- It Sets up a MySQL server in preparation for the projects.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
