deffa57ee5a796a2b29d7d1c925a0c189d7b1188
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-04-21 12:56:03 -0700

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""this intializes Flask web app
-"""
+""" this starts the flask web app """
+
 from models import storage
 from flask import Flask
 from flask import render_template
 
-res = Flask(__name__)
+app = Flask(__name__)
 
 
-@res.route("/states_list", strict_slashes=False)
-def fun():
-    """this shows html page of state alphabetically.
-    """
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ it displays state name alphabetically """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
-@res.teardown_appcontext
-def fun(xy):
-    """this exits sql session"""
+@app.teardown_appcontext
+def teardown(exc):
+    """it closes current session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    res.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
