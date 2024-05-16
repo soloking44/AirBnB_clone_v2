8f0a73158b7651263d6f8eaec114213178d5eaea
soloking44 <solop8483@gmail.com>
2024-05-16 08:08:12 -0700

    update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-<<<<<<< HEAD
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
@@ -15,13 +14,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-=======
-@app.route("/airbnb-onepage", strict_slashes=False)
-def index():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port='5000')
->>>>>>> origin/app-server
